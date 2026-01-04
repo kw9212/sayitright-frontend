a013dbea3b815f31773619eea24714eafa405f5d
Keunwoo <104286190+kw9212@users.noreply.github.com>
2026-01-04 21:20:53 +0900

    chore: apply prettier setting to current files

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -6,9 +6,5 @@ import { ReactNode, useState } from 'react';
 export default function Providers({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-    </QueryClientProvider>
-  );
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
